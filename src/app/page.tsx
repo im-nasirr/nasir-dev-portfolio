bc4fdc12817a8a6aea643ba4f5175e2e763e0f1f
im-nasirr <mramzannasir555@gmail.com>
2025-06-10 12:55:37 +0500

    Refactor Home component layout and remove HomepageBlogs section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,40 +26,40 @@ export default async function Home() {
     const blogs = await fetchBlogs();
 
     return (
-        <main className="w-full min-h-screen px-4 sm:px-20 xl:px-40 2xl:px-80">
-            <script
-                type="application/ld+json"
-                dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-            />
-            <section className="w-full flex flex-wrap">
-                <header className="w-full lg:h-[100dvh] lg:sticky top-0 lg:w-1/2 pt-40 lg:pb-40 flex flex-col lg:justify-between">
-                    <div>
-                        <HeroTitle />
-                        <TitleList titles={siteConfig.titles} />
-                        <DynamicTagline
-                            taglines={siteConfig.taglines}
-                            className="pl-1 text-center lg:text-start text-xs lg:text-sm mt-4 min-h-20 max-w-[500px] text-slate-350"
-                        />
-                        <TableOfContents />
-                        <HireMeButton isSticky />
-                    </div>
-                    <div>
-                        <SocialIcons isSticky />
-                        <Footer isSticky />
-                    </div>
-                </header>
-                <main className="w-full lg:w-1/2 px-4 sm:px-0 pt-20 lg:pt-40 text-slate-350 text-sm flex flex-wrap justify-center lg:block lg:mb-40">
-                    <AboutMe />
-                    <Experiences />
-                    <Projects />
-                    <Technologies />
-                    <HomepageBlogs blogs={blogs} />
-                    <ContactForm />
-                    <Footer />
-                    <SocialIcons />
-                    <OpenToWorkBanner />
-                </main>
-            </section>
-        </main>
+      <main className="w-full min-h-screen px-4 sm:px-20 xl:px-40 2xl:px-80">
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+        <section className="w-full flex flex-wrap">
+          <header className="w-full lg:h-[100dvh] lg:sticky top-0 lg:w-1/2 pt-40 lg:pb-40 flex flex-col lg:justify-between">
+            <div>
+              <HeroTitle />
+              <TitleList titles={siteConfig.titles} />
+              <DynamicTagline
+                taglines={siteConfig.taglines}
+                className="pl-1 text-center lg:text-start text-xs lg:text-sm mt-4 min-h-20 max-w-[500px] text-slate-350"
+              />
+              <TableOfContents />
+              <HireMeButton isSticky />
+            </div>
+            <div>
+              <SocialIcons isSticky />
+              <Footer isSticky />
+            </div>
+          </header>
+          <main className="w-full lg:w-1/2 px-4 sm:px-0 pt-20 lg:pt-40 text-slate-350 text-sm flex flex-wrap justify-center lg:block lg:mb-40">
+            <AboutMe />
+            <Experiences />
+            <Projects />
+            <Technologies />
+            {/* <HomepageBlogs blogs={blogs} /> */}
+            <ContactForm />
+            <Footer />
+            <SocialIcons />
+            <OpenToWorkBanner />
+          </main>
+        </section>
+      </main>
     );
 }
